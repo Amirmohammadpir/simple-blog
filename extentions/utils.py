e753ce2ccde5704db442bc1e73df82af2bef4f9d
Amirmohammad pir <amirmohammadpir@gmail.com>
2022-03-15 15:27:37 +0330

    solve problem diplay name of persian months

--- extentions/utils.py
+++ extentions/utils.py
@@ -22,16 +22,16 @@ def jalali_converter(time):
   time_to_str = "{},{},{}".format(time.year, time.month,time.day)
   time_to_tuple = jalali.Gregorian(time_to_str).persian_tuple()
   time_to_list = list(time_to_tuple)
-  
+
   for index, month in enumerate(jmonth):
     if time_to_list[1] == index + 1 :
       time_to_list[1] = month
       break
 
   output = '{} {} {} , ساعت {}:{}'.format(
-    time_to_tuple[2],
-    time_to_tuple[1],
-    time_to_tuple[0],
+    time_to_list[2],
+    time_to_list[1],
+    time_to_list[0],
     time.hour,
     time.minute,
   )
