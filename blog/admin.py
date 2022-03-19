077e57ebb2b56831dff9b45d1b6e74ea5984185e
Amirmohammad pir <amirmohammadpir@gmail.com>
2022-03-19 11:26:15 +0330

    added category to blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from .models import Article 
+from .models import Article , Category
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('position','title', 'slug', 'status')
+  list_filter = (['status'])
+  search_fields = ('title', 'slug')
+  prepopulated_field = {'slug': ('title',)}
+
+  
+admin.site.register(Category, CategoryAdmin)
+
 class ArticleAdmin(admin.ModelAdmin):
-  list_display = ('title', 'slug', 'jpublished', 'status')
+  list_display = ('title', 'slug', 'jpublished','category_to_str', 'status')
   list_filter = ('status','published')
   search_fields = ('title', 'description')
   prepopulated_field = {'slug': ('title',)}
   ordering = ['-status', '-published']
+
+  def category_to_str(self, obj):
+    return 'Categories'
+
 admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,30 @@ from django.db import models
 from django.utils import timezone
 from extentions.utils import jalali_converter
 
+class Category(models.Model):
+  title = models.CharField(max_length=200, verbose_name='عنوان')
+  slug = models.SlugField(unique=True, verbose_name="آدرس")
+  status = models.BooleanField(default=True, verbose_name="نمایش داده شود ؟")
+  position = models.IntegerField(verbose_name='پوزیشن')
+
+  class Meta:
+    verbose_name = "دسته بندی"
+    verbose_name_plural = "دسته بندی ها"
+    ordering = ['position']
+
+  def __str__(self):
+    return self.title
+
+
 class Article(models.Model):
   STATUS_CHOICE = [
     ("p" , "منتشر شده"),
     ('d' , 'پیش‌نویس'),
   ]
+
   title = models.CharField(max_length=200, verbose_name='عنوان')
   slug = models.SlugField(unique=True, verbose_name="آدرس")
+  category = models.ManyToManyField(Category, verbose_name='دسته بندی')
   content = models.TextField(verbose_name="محتوا")
   published = models.DateTimeField(verbose_name="تاریخ انشتار")
   thumbnail = models.ImageField(upload_to='images/',default='default.jpg', verbose_name="عکس")
