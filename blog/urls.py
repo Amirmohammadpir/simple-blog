a15f65247336b454f8a1a3dde11adbbda2272fa3
Amirmohammad pir <amirmohammadpir@gmail.com>
2022-03-23 15:52:45 +0430

    pagination is created and now category have pagination

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import home , detail, category
 urlpatterns = [
   path('', home, name='home'),
+  path('page/<int:page>', home, name='home'),
   path('detail/<slug:slug>', detail, name='detail'),
   path('category/<slug:slug>', category, name='category'),
+  path('category/<slug:slug>/page/<int:page>', category, name='category'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, get_list_or_404
 from .models import Article, Category
 
-def home(request):
-
-  context = {'articles_list': Article.objects.published(),}
+def home(request, page=1):
+  articles_list = Article.objects.published()
+  paginator = Paginator(articles_list,3)
+  articles = paginator.get_page(page)
+  context = {'articles_list': articles }
 
   return render(request, 'blog/index.html', context)
 
@@ -16,8 +19,14 @@ def detail(request, slug):
   return render(request, 'blog/post.html', context)
 
 
-def category(request, slug):
-  
-  context= {'category': get_object_or_404(Category, slug=slug, status=True)}
+def category(request, slug, page=1):
+  category = get_object_or_404(Category, slug=slug, status=True)
+  articles_list = category.articles.published()
+  paginator = Paginator(articles_list,3)
+  articles = paginator.get_page(page)
+  context= {
+    'category': category,
+    'articles': articles,
+   }
 
   return render(request, 'blog/category.html', context)
